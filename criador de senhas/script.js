dbcaa931da44e312eeae20be930e49f94674a8ea
Carl0sC3sar <carloscesarbr1504@gmail.com>
2023-03-24 13:29:29 -0300

    atualização

--- criador de senhas/script.js
+++ criador de senhas/script.js
@@ -4,25 +4,25 @@ let buttonElement = document.querySelector("#button")
 let sizePassword= document.querySelector("#valor")
 let password = document.querySelector("#password")
 
-let containerPassword= document.querySelector("#containerPassword")
+let containerPassword= document.querySelector("#container-password")
 
 let charset = "abcdefghijklmnopqrstuvxwyz1234567890";
 let novaSenha= "";
 
 sizePassword.innerHTML= sliderElement.value;
-slider.oniput = function(){
+slider.oninput = function(){
     sizePassword.innerHTML = this.value;
 }
 function generatePassword(){
     
-        let pass = "";
-        for (let i=0, n = charset.length; i < sliderElement.value; i++)
+        let pass = '';
+        for (let i=0, n = charset.length; i < sliderElement.value; ++i)
         pass += charset.charAt(Math.floor(Math.random()* n));
 
     }
     console.log(pass)
    containerPassword.classlist.remove("hide")
-   password.innerHTML * pass;
+   password.innerHTML = pass;
    novaSenha= pass;
 
    function copyPassword(){
